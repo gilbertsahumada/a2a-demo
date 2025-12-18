57eccedec5beac7afb411b0507d5a4d07dc51802
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-12-17 21:15:00 -0500

    creating client for a2a example

--- client.ts
+++ client.ts
@@ -1,4 +1,4 @@
-import { MessageSendParams } from "@a2a-js/sdk";
+import { MessageSendParams, SendMessageResponse } from "@a2a-js/sdk";
 import { A2AClient } from "@a2a-js/sdk/client";
 import { v4 } from "uuid";
 
@@ -6,7 +6,10 @@ async function sendText(url: string, text: string) {
     const client = new A2AClient(url);
     const params: MessageSendParams = {
         message: { 
-            
+            kind: "message",
+            role: "user",
+            messageId: v4(),
+            parts: [{ kind: "text", text }]
         },
         configuration: {
             blocking: true,
@@ -14,5 +17,7 @@ async function sendText(url: string, text: string) {
         }
     }
 
-    const response = await client.sendMessage(params);
+    const response: SendMessageResponse = await client.sendMessage(params);
+    const msg = response.result;
+    return msg?.parts?.[0] ?? "";
 }
